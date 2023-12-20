7b70afd0cfd256bac5399cc97879b0f9d0d84747
Andrei Guzei <aaguzey@gmail.com>
2023-12-20 12:14:53 +1000

    init

---
+++ studentaccounting/src/test/java/com/example/studentaccounting/StudentaccountingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentaccounting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentaccountingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
