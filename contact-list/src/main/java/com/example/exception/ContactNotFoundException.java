634e47ef46e237cf80a6f9b1cc69f86808851295
Andrei Guzei <aaguzey@gmail.com>
2024-04-02 01:44:02 +1000

    Added a custom exception that is thrown when no contact is found

---
+++ contact-list/src/main/java/com/example/exception/ContactNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.exception;
+
+public class ContactNotFoundException extends RuntimeException {
+
+    public ContactNotFoundException(String message) {
+        super(message);
+    }
+}
